c3b8687d7c04617cb7f450228d8bc83793986be4
DevExpressExampleBot <fakeEmail>
2022-12-17 00:58:47 +0400

    Source auto update [skip ci]

--- VB/WpfApplication25/ViewModel.vb
+++ VB/WpfApplication25/ViewModel.vb
@@ -1,18 +1,17 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports DevExpress.Mvvm
 
 Namespace WpfApplication25
+
     Public Class ViewModel
-        Public Overridable Property Text() As String
-        Public Overridable ReadOnly Property MessageBoxService() As IMessageBoxService
+
+        Public Overridable Property Text As String
+
+        Public Overridable ReadOnly Property MessageBoxService As IMessageBoxService
             Get
                 Return Nothing
             End Get
         End Property
+
         Public Sub ShowText()
             MessageBoxService.ShowMessage(Text)
         End Sub
